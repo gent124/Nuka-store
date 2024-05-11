c1dace404e8c9ffeac539dbd9c3bd894cca07040
gent124 <gr47048@ubt-uni.net>
2024-05-11 18:23:54 +0200

    Deploy 6

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://inventory-management-system-lll1d3sg4-gent124s-projects.vercel.app/';
+axios.defaults.baseURL = 'https://inventory-management-system-lll1d3sg4-gent124s-projects.vercel.app';
 axios.defaults.withCredentials = true;
 
 export default axios;
